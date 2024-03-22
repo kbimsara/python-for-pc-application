3913d090cb1ba074a526bdbec0ce2cc4ebcedba8
Kavindu Bimsara <34527634+kbimsara@users.noreply.github.com>
2024-03-22 13:50:09 +0530

    update

--- Calculator/main.py
+++ Calculator/main.py
@@ -12,26 +12,49 @@ main_window.resize(250,300)
 txt_box=QLineEdit()
 grid=QGridLayout()
 
-btn=["7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","=",]
+btn=["7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+",]
 
 clear=QPushButton("Clear")
 delete=QPushButton("<")
 
+# function
+def click():
+    new_btn=app.sender()
+    txt=new_btn.text()
+    # txt_box.setText(str(txt)) #we can see witch button click
+
+    if txt=="=":
+        symbol=txt_box.text()
+        try:
+            result=eval(symbol)
+            txt_box.setText(str(result))
+        except Exception as e:
+            print("Error :",e)
+    elif txt=="clear":
+        txt_box.clear()
+    elif txt=="<":
+        current_val=txt_box.text()
+        txt_box.setText(current_val[:-1])
+    else:
+        current_val=txt_box.text()
+        txt_box.setText(current_val+txt)
+
 row=0
 col=0
+
 for txt in btn:
-    new_btn=QPushButton(txt)
-    # new_btn.clicked.connect()
+    new_btn=QPushButton(txt)   
+    new_btn.clicked.connect(click)
+    grid.addWidget(new_btn,row,col)
     col+=1
     if col >3:
         col=0
         row+=1
     
-
 # desing of application
 master_layout=QVBoxLayout()
 master_layout.addWidget(txt_box)
-# master_layout.addLayout(grid)
+master_layout.addLayout(grid)
 
 btn_row=QHBoxLayout()
 btn_row.addWidget(clear)
@@ -40,9 +63,10 @@ btn_row.addWidget(delete)
 master_layout.addLayout(btn_row)
 main_window.setLayout(master_layout)
 
-# function
 
 # events
+clear.clicked.connect(click)
+delete.clicked.connect(click)
 
 # show and run
 main_window.show()
